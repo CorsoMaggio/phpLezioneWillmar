d7e92590c82e1d1fcb66336f8c667caf298ef50c
willmar83 <willmarrengifo8@gmail.com>
2025-06-10 18:13:04 +0200

    8 commit

---
+++ esercizio5.php
@@ -0,0 +1,19 @@
+<?php
+
+for ($x = 0; $x <= 30; $x++) {
+    $modulo = 0;
+    $modulo3 = $x % 3;
+    $modulo5 = $x % 5;
+    if($x % 3 == 0){
+          if($x % 5 == 0){
+            echo "Francesco - ";
+         }
+         else{
+            echo "PHP - ";
+         }
+    }elseif($x % 5 == 0){
+            echo "JAVASCRIPT -";
+         }
+
+}
+?>
