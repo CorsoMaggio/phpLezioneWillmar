9f363f7c027a3eab0f3dd1c39a311dd3d654e259
willmar83 <willmarrengifo8@gmail.com>
2025-06-10 14:53:00 +0200

    4 commit

---
+++ esercizio2.php
@@ -0,0 +1,21 @@
+<?php
+// ottenere l'età
+// La console deve dire che hai accesso alla piattaforma se hai più di 4 anni
+
+echo "Inserisci il tuo anno di nascità: ";
+$anoNascita = (int) trim(fgets(STDIN));
+echo "Inserisci l'anno attuale: ";
+$anoOggi = (int) trim(fgets(STDIN));
+
+$anniTotale = $anoOggi - $anoNascita;
+
+if ($anoNascita > $anoOggi) {
+    echo 'I dati inseriti non sono corretti';
+} else if ($anniTotale >= 4) {
+    echo 'Puoi accedere alla piattaforma perchè hai: ' . $anniTotale . ' anni.';
+} else {
+    echo "Non puoi accedere alla piattaforma, devi avere almeno 4 anni";
+}
+
+
+?>
